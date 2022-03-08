2a313940342817406656cf9144ed125caf54c00c
Stefanus Albert Kosim <albertkosim24@gmail.com>
2022-03-08 20:37:24 +0700

    Add project files.

---
+++ PBKK_Quiz_1/Pages/q1/collaboration.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PBKK_Quiz_1.Pages.q1.collaboration
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
